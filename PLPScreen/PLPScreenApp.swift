ccf8b31c2c4d8e38487a5ada66d710586301078a
Bharat Singh <bharat.s@fnp.com>
2026-02-26 12:21:19 +0530

    Initial Commit

---
+++ PLPScreen/PLPScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PLPScreenApp.swift
+//  PLPScreen
+//
+//  Created by Bharat Singh  on 26/02/26.
+//
+
+import SwiftUI
+
+@main
+struct PLPScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
